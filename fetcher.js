4ca3353ff94890db20ba279a4137af883a8ff547
Akhila499 <akhilathota499@gmail.com>
2021-07-28 23:51:55 -0400

    minor changes in the code

--- fetcher.js
+++ fetcher.js
@@ -2,11 +2,11 @@ const request = require('request');
 const fs = require('fs');
 
 const input = process.argv.slice(2);
-console.log(typeof input[1]);
+//console.log(typeof input[1]);
 request(input[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
   const content = body;
   fs.writeFile(input[1],content, err =>{
     if(err){
